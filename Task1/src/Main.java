06121cbaa02170449093dc50ebffb7ae648eae57
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-12-02 19:02:32 +0300

    1

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -3,7 +3,16 @@ public class Main {
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+    private static final String RUS_ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
     public static void main(String[] args) {
+        printCodeChar(RUS_ALPHABET);
         //todo код писать тут
+    }
+
+    private static void printCodeChar(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            System.out.println(string.charAt(i)+" - "+(int) string.charAt(i));
+        }
     }
 }
